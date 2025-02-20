fadd51ed36c6f5d2398273921827f10230a96215
Dru-429 <ect.dhruv@gmail.com>
2025-02-20 23:14:17 +0530

    Adding images

---
+++ app/components/parallaxScroll/ParallaxScroll.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ParallaxScroll = () => {
+
+    const images = [
+        "1.jpeg",
+        "2.jpeg",
+        "3.jpeg",
+        "4.jpeg",
+        "5.jpeg",
+        "6.jpeg",
+        "7.jpeg",
+        "8.jpeg",
+        "9.jpeg",
+        "10.jpeg",
+        "11.jpeg",
+        "12.jpeg",
+    ]
+
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ParallaxScroll
