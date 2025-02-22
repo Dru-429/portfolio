5f608419f02348a7b9a69e77bfdbb75b88ffe8bc
Dru-429 <ect.dhruv@gmail.com>
2025-02-22 11:14:51 +0530

    Projects

--- app/page.jsx
+++ app/page.jsx
@@ -10,6 +10,7 @@ import TeachStack from './components/techstack/Techstack.jsx';
 import Services from './components/servi/Services.jsx';
 import ParallaxScroll from './components/parallaxScroll/ParallaxScroll.jsx';
 import Contact from './components/contact/Contact.jsx';
+import Projects from './components/projects/Projects.jsx';
 
 
 const page = () => {
@@ -21,6 +22,7 @@ const page = () => {
       <HeroSection />
       <About />
       <TeachStack />
+      {/* <Projects /> */}
       <Services />
       {/* <ParallaxScroll /> */}
       <Contact />
