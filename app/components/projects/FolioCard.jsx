d31fa0ba66680b0810404f8fc5d1691dcbbf729d
Dru-429 <ect.dhruv@gmail.com>
2025-02-26 22:32:37 +0530

    Added projects section ixed version

--- app/components/projects/FolioCard.jsx
+++ app/components/projects/FolioCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { motion } from "framer-motion";
-import { FaGithub, FaExternalLinkAlt } from "react-icons/fa"
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 import TechCapsule from "../ui/TechCapsule";
 import AnimateTitle3 from "../ui/AnimateTitle3";
 
@@ -17,82 +17,76 @@ export default function FolioCard({
   stack,
 }) {
   return (
-    
-    <AnimateTitle3 
-    key={index}
-    title={title} 
-    sno={index}
-    child={
-      <motion.div
-      className="bg-[#fde1a922] w-full sm:w-[90%] rounded-2xl overflow-hidden shadow-lg flex flex-col justify-between items-center gap-10 sm:flex-row p-5 mb-5 hover:shadow-[5px_5px_10px_rgba(0, 0, 0, 0.2)] transition-shadow duration-200"
-      whileHover={{ scale: 1.005 }}
-      transition={{ duration: 0.3 }}
-    >
-      <div className="relative">
-        <Image 
-          src={img} 
-          alt={title} 
-          width={520}
-          height={800}
-          objectFit="cover" 
-        />
-      </div>
+    <AnimateTitle3
+      key={index}
+      title={title}
+      sno={index}
+      child={
+        <motion.div
+          className="bg-[#fde1a922] rounded-t-2xl overflow-hidden shadow-lg flex flex-col justify-between items-center gap-10 sm:flex-row p-5 mb-5 hover:shadow-[5px_5px_10px_rgba(0, 0, 0, 0.2)] transition-shadow duration-200"
+          whileHover={{ scale: 1.005 }}
+          transition={{ duration: 0.3 }}
+        >
+          <div className="p-6">
+            {/* //links */}
+            <div className="flex justify-end items-end mb-4">
+              <h2 className="text-2xl font-semibold text-primary sm:hidden ">{title}</h2>
+              <div className="flex gap-4">
+                {gitLink && (
+                  <div className="text-text text-2xl hover:text-secondary hover:scale-110 duration-200">
+                    <Link
+                      href={gitLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaGithub className="text-text text-2xl hover:text-secondary" />
+                    </Link>
+                  </div>
+                )}
 
-      <div className="p-6">
-
-        {/* //links */}
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-2xl font-semibold text-primary">{title}</h2>
-
-
-          <div className="flex gap-4">
-            {gitLink && (
-              <div
-                className="text-text text-2xl hover:text-secondary hover:scale-110 duration-200"
-              >
-                <Link href={gitLink} target="_blank" rel="noopener noreferrer">
-                  <FaGithub className="text-text text-2xl hover:text-secondary" />
-                </Link>
+                {liveLink && (
+                  <motion.div
+                    className="text-text text-2xl hover:text-secondary hover:scale-110 duration-200"
+                    whileTap={{ scale: 1.0 }}
+                  >
+                    <Link
+                      href={liveLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaExternalLinkAlt />
+                    </Link>
+                  </motion.div>
+                )}
               </div>
-            )}
+            </div>
 
-            {liveLink && (
-              <motion.div 
-                className="text-text text-2xl hover:text-secondary hover:scale-110 duration-200"
-                whileTap={{scale: 1.0 }}
-              > 
-                <Link 
-                  href={liveLink} 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <FaExternalLinkAlt />
-                </Link>
-              </motion.div>
-            )}
+            <p className="text-text mb-4">{about}</p>
+
+            <div className="flex flex-wrap gap-2">
+              {stack.map((tech, index) => (
+                // <span
+                //   key={index}
+                //   className="bg-gray-800 text-gray-300 px-3 py-1 rounded-full text-sm"
+                // >
+                //   {tech}
+                // </span>
+
+                <TechCapsule index={index} item={tech} />
+              ))}
+            </div>
           </div>
-        </div>
-
-        <p className="text-text mb-4">{about}</p>
-
-        <div className="flex flex-wrap gap-2">
-          {stack.map((tech, index) => (
-            // <span
-            //   key={index}
-            //   className="bg-gray-800 text-gray-300 px-3 py-1 rounded-full text-sm"
-            // >
-            //   {tech}
-            // </span>
-
-            <TechCapsule 
-              index={index}
-              item={tech}
+          <div className="relative">
+            <Image
+              src={img}
+              alt={title}
+              width={520}
+              height={800}
+              objectFit="cover"
             />
-          ))}
-        </div>
-      </div>
-      </motion.div>
-    }
+          </div>
+        </motion.div>
+      }
     />
   );
 }
--- app/components/ui/AnimateTitle3.jsx
+++ app/components/ui/AnimateTitle3.jsx
@@ -1,181 +1,188 @@
 import React from "react";
-import { AnimatePresence, easeIn, easeOut, motion, useAnimationControls } from 'framer-motion';
+import {
+  AnimatePresence,
+  easeIn,
+  easeOut,
+  motion,
+  useAnimationControls,
+} from "framer-motion";
 
-const AnimateTitle3 = ({sno, title, child}) => {
-    
-    const controlTop = useAnimationControls()
-    const controlBottom = useAnimationControls()
-    // const controlClicked = useAnimationControls()
-    const controlLine = useAnimationControls()
-    const Sno = "0" + ` ${sno}`
+const AnimateTitle3 = ({ sno, title, child }) => {
+  const controlTop = useAnimationControls();
+  const controlBottom = useAnimationControls();
+  // const controlClicked = useAnimationControls()
+  const controlLine = useAnimationControls();
+  const Sno = "0" + ` ${sno + 1}`;
 
-    const handelEnter = () => { 
-        controlTop.start({
-            y: "-100%"
-        })
+  const handelEnter = () => {
+    controlTop.start({
+      y: "-100%",
+    });
 
-        controlBottom.start({
-            y: "100%"
-        })
+    controlBottom.start({
+      y: "100%",
+    });
 
-        controlLine.start({
-            width: "100%"
-        })
-    }
+    controlLine.start({
+      width: "100%",
+    });
+  };
 
-    const handelExit = () => {
-        console.log("Mouse Exit")
-        controlTop.start({
-            y: "0%",
-        })
-        controlBottom.start({
-            y: "0%"
-        })
-        controlLine.start({
-            width: "0%"
-        })
-    }
+  const handelExit = () => {
+    console.log("Mouse Exit");
+    controlTop.start({
+      y: "0%",
+    });
+    controlBottom.start({
+      y: "0%",
+    });
+    controlLine.start({
+      width: "0%",
+    });
+  };
 
   return (
-
-    <div 
-        className="flex relative"
-        onMouseEnter={handelEnter}
-        onMouseLeave={handelExit}
+    <div
+      className="flex relative w-full sm:w-[90%]"
+      onMouseEnter={handelEnter}
+      onMouseLeave={handelExit}
     >
-      <div className="flex relative top-1">
-        {Sno.split(" ").map((item, index) => {
-          if (index == 0 || index % 2 == 0) {
-            return (
-              <span
-                className="text-[12px] relative tracking-tighter h-[12px] flex flex-col overflow-y-hidden"
-                key={index}
-              >
-                <motion.div
-                  initial={{ top: 0, opacity: 1 }}
-                  animate={controlTop}
-                  transition={{ duration: 0.5, ease: easeIn }}
-                >
-                  {item}
-                </motion.div>
-                <motion.div
-                  initial={{ top: 0, opacity: 1 }}
-                  animate={controlTop}
-                  transition={{ duration: 0.5, ease: easeIn }}
-                >
-                  {item}
-                </motion.div>
-                <motion.div
-                  initial={{ top: 0, opacity: 1 }}
-                  animate={controlTop}
-                  transition={{ duration: 0.5, ease: easeIn }}
-                >
-                  {item}
-                </motion.div>
-              </span>
-            );
-          } else {
-            return (
-              <span
-                className="text-[12px] relative h-[12px] tracking-tighter flex flex-col overflow-y-hidden"
-                key={index}
-              >
-                <motion.div
-                  className="absolute -top-[12px]"
-                  initial={{ y: "-100%", opacity: 1 }}
-                  animate={controlBottom}
-                  transition={{ duration: 0.5, ease: easeIn }}
-                >
-                  {item}
-                </motion.div>
-                <motion.div
-                  initial={{ y: "-100%", opacity: 1 }}
-                  animate={controlBottom}
-                  transition={{ duration: 0.5, ease: easeIn }}
-                >
-                  {item}
-                </motion.div>
-                <motion.div
-                  initial={{ y: "-100%", opacity: 1 }}
-                  animate={controlBottom}
-                  transition={{ duration: 0.5, ease: easeIn }}
-                >
-                  {item}
-                </motion.div>
-              </span>
-            );
-          }
-        })}
-      </div>
+      <div className="hidden md:flex gap-1 absolute top-10 left-10  ">
 
-      <div className="flex relative">
-        {title.split(" ").map((item, index) => {
-          if (index == 0 || index % 2 == 0) {
-            return (
-              <span
-                className="text-2xl font-semibold text-primary relative h-[32px] flex flex-col overflow-y-hidden pl-1"
-                key={index}
-              >
-                    <motion.div
+        <div className="flex relative top-1">
+          {Sno.split(" ").map((item, index) => {
+            if (index == 0 || index % 2 == 0) {
+              return (
+                <span
+                  className="text-[12px] relative tracking-tighter h-[14px] flex flex-col overflow-y-hidden -pt-1"
+                  key={index}
+                >
+                  <motion.div
+                    initial={{ top: 0, opacity: 1 }}
+                    animate={controlTop}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                  <motion.div
+                    initial={{ top: 0, opacity: 1 }}
+                    animate={controlTop}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                  <motion.div
+                    initial={{ top: 0, opacity: 1 }}
+                    animate={controlTop}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                </span>
+              );
+            } else {
+              return (
+                <span
+                  className="text-[12px] relative h-[14px] tracking-tighter flex flex-col overflow-y-hidden"
+                  key={index}
+                >
+                  <motion.div
+                    className="absolute -top-[18px]"
+                    initial={{ y: "-100%", opacity: 1 }}
+                    animate={controlBottom}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                  <motion.div
+                    initial={{ y: "-100%", opacity: 1 }}
+                    animate={controlBottom}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                  <motion.div
+                    initial={{ y: "-100%", opacity: 1 }}
+                    animate={controlBottom}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                </span>
+              );
+            }
+          })}
+        </div>
+
+        <div className="flex relative">
+          {title.split(" ").map((item, index) => {
+            if (index == 0 || index % 2 == 0) {
+              return (
+                <span
+                  className="text-2xl font-semibold text-primary relative h-[32px] flex flex-col overflow-y-hidden pl-1"
+                  key={index}
+                >
+                  <motion.div
                     className="relative "
                     initial={{ top: 0, opacity: 1 }}
                     animate={controlTop}
                     transition={{ duration: 0.5, ease: easeIn }}
-                    >
+                  >
                     {item}
-                    </motion.div>
-                    <motion.div
+                  </motion.div>
+                  <motion.div
                     className="relative "
                     initial={{ top: 0, opacity: 1 }}
                     animate={controlTop}
                     transition={{ duration: 0.5, ease: easeIn }}
-                    >
+                  >
                     {item}
-                    </motion.div>
-                    <motion.div
+                  </motion.div>
+                  <motion.div
                     className="relative "
                     initial={{ top: 0, opacity: 1 }}
                     animate={controlTop}
                     transition={{ duration: 0.5, ease: easeIn }}
-                    >
+                  >
                     {item}
-                    </motion.div>
-              </span>
-            );
-          } else {
-            return (
-              <span
-                className="text-2xl font-semibold text-primary relative h-[32px] flex flex-col overflow-y-hidden pl-1"
-                key={index}
-              >
-                <motion.div
-                  className="absolute -top-[33px]"
-                  initial={{ opacity: 1 }}
-                  animate={controlBottom}
-                  transition={{ duration: 0.5, ease: easeIn }}
+                  </motion.div>
+                </span>
+              );
+            } else {
+              return (
+                <span
+                  className="text-2xl font-semibold text-primary relative h-[32px] flex flex-col overflow-y-hidden pl-1"
+                  key={index}
                 >
-                  {item}
-                </motion.div>
-                <motion.div
-                  className="relative "
-                  initial={{ y: "-100%", opacity: 1 }}
-                  animate={controlBottom}
-                  transition={{ duration: 0.5, ease: easeIn }}
-                >
-                  {item}
-                </motion.div>
-                <motion.div
-                  className="relative "
-                  initial={{ y: "-100%", opacity: 1 }}
-                  animate={controlBottom}
-                  transition={{ duration: 0.5, ease: easeIn }}
-                >
-                  {item}
-                </motion.div>
-              </span>
-            );
-          }
-        })}
+                  <motion.div
+                    className="absolute -top-[33px]"
+                    initial={{ opacity: 1 }}
+                    animate={controlBottom}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                  <motion.div
+                    className="relative "
+                    initial={{ y: "-100%", opacity: 1 }}
+                    animate={controlBottom}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                  <motion.div
+                    className="relative "
+                    initial={{ y: "-100%", opacity: 1 }}
+                    animate={controlBottom}
+                    transition={{ duration: 0.5, ease: easeIn }}
+                  >
+                    {item}
+                  </motion.div>
+                </span>
+              );
+            }
+          })}
+        </div>
       </div>
 
       {/* //Pluse Icon */}
@@ -189,14 +196,15 @@ const AnimateTitle3 = ({sno, title, child}) => {
         ></motion.div>
       </div> */}
 
-      <motion.div
-        className="w-full absolute h-[0.5px] bg-zinc-800 -bottom-3"
-        initial={{ width: "0%" }}
-        animate={controlLine}
-        transition={{ duration: 0.7, ease: easeOut }}
-      ></motion.div>
-
-      {child}
+      <div>
+        <motion.div
+          className="w-full absolute h-[5px] bg-secondary bottom-4 left-0 scale-100"
+          initial={{ width: "0%" }}
+          animate={controlLine}
+          transition={{ duration: 0.7, ease: easeOut }}
+        ></motion.div>
+        {child}
+      </div>
     </div>
   );
 };
