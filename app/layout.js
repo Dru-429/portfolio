2964a00b9c22bd9fbcbe092d720351c74bc5ef2c
Dru-429 <ect.dhruv@gmail.com>
2025-07-02 21:04:24 +0530

    Meta-tag paused

--- app/layout.js
+++ app/layout.js
@@ -43,46 +43,46 @@ export const metadata = {
     "software engineer",
     "portfolio",
   ],
-  openGraph: {
-    title: "Adeola Badero — Frontend Software Engineer",
-    description:
-      "Frontend Engineer formerly at Bluechip Technologies, passionate about crafting next-gen software and creative websites. I drive growth by crafting user experiences that blend aesthetics with functionality through my design and development skills. I thrive on turning ideas into seamless digital solutions.",
-    url: "https://www.adeolabadero.me",
-    siteName: "www.adeolabadero.me",
-    images: [
-      {
-        url: "https://i.ibb.co/FKMqc28/adeola-badero.png",
-        width: 1200,
-        height: 630,
-        alt: "Adeola Badero — Frontend Software Engineer",
-      },
-      {
-        url: "https://i.ibb.co/Y8hBTR4/ade-800.png",
-        width: 800,
-        height: 800,
-        alt: "Adeola Badero — Frontend Software Engineer",
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Adeola Badero — Software Engineer",
-    description:
-      "Frontend Engineer at Bluechip Technologies, passionate about crafting next-gen software. I drive growth by crafting user experiences that blend aesthetics with functionality through my design and development skills. I thrive on turning ideas into seamless digital solutions.",
-    creator: "@Ade_the_great",
-    images: ["https://i.ibb.co/FKMqc28/adeola-badero.png"],
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-    googleBot: {
-      index: true,
-      "max-image-preview": "large",
-    },
-  },
+  // openGraph: {
+  //   title: "Adeola Badero — Frontend Software Engineer",
+  //   description:
+  //     "Frontend Engineer formerly at Bluechip Technologies, passionate about crafting next-gen software and creative websites. I drive growth by crafting user experiences that blend aesthetics with functionality through my design and development skills. I thrive on turning ideas into seamless digital solutions.",
+  //   url: "https://www.adeolabadero.me",
+  //   siteName: "www.adeolabadero.me",
+  //   images: [
+  //     {
+  //       url: "https://i.ibb.co/FKMqc28/adeola-badero.png",
+  //       width: 1200,
+  //       height: 630,
+  //       alt: "Adeola Badero — Frontend Software Engineer",
+  //     },
+  //     {
+  //       url: "https://i.ibb.co/Y8hBTR4/ade-800.png",
+  //       width: 800,
+  //       height: 800,
+  //       alt: "Adeola Badero — Frontend Software Engineer",
+  //     },
+  //   ],
+  //   locale: "en-US",
+  //   type: "website",
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Adeola Badero — Software Engineer",
+  //   description:
+  //     "Frontend Engineer at Bluechip Technologies, passionate about crafting next-gen software. I drive growth by crafting user experiences that blend aesthetics with functionality through my design and development skills. I thrive on turning ideas into seamless digital solutions.",
+  //   creator: "@Ade_the_great",
+  //   images: ["https://i.ibb.co/FKMqc28/adeola-badero.png"],
+  // },
+  // robots: {
+  //   index: true,
+  //   follow: true,
+  //   nocache: false,
+  //   googleBot: {
+  //     index: true,
+  //     "max-image-preview": "large",
+  //   },
+  // },
   category: "technology",
 };
 
